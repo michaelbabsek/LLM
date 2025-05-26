fixed immense bug with cross entropy loss for correct loss calculation! credits to @chatgpt

dataset.py
@@ -49,7 +49,7 @@ def _prepare():  # source: https://github.com/karpathy/nanoGPT/blob/master/data/
         arr.flush()
 
 class BinDataset(Dataset):
-    def __init__(self, chunk_size: int, split: str, device: Optional[torch.device] = "cpu"):
+    def __init__(self, chunk_size: int, split: str, device = "cpu"):
         super().__init__()
         self.data = np.memmap(filename=f"{split}.bin", dtype=np.uint32, mode="r")
         self.chunk_size = chunk_size
@@ -59,10 +59,9 @@ class BinDataset(Dataset):
         return len(self.data) - self.chunk_size
 
     def __getitem__(self, idx):
-        x =  torch.from_numpy(self.data[idx:idx + self.chunk_size].astype(np.int64))
-        y = torch.from_numpy(self.data[idx+1:idx + self.chunk_size+1].astype(np.int64))
-
-        return x.to(device=self.device, non_blocking=True), y.to(device=self.device, non_blocking=True)
+        x = self.data[idx      : idx + self.chunk_size    ].astype(np.int64, copy=False)
+        y = self.data[idx + 1  : idx + self.chunk_size + 1].astype(np.int64, copy=False)
+        return torch.from_numpy(x), torch.from_numpy(y)
 
 if __name__ == "__main__":
     _prepare()

model.py
@@ -22,7 +22,7 @@ class MultiheadAttention(nn.Module):
         self.qkv = nn.Linear(args.n_dim, args.n_dim * 3)
         self.dropout = 0.1
 
-    def forward(self, x, mask: Optional[torch.Tensor] = None):
+    def forward(self, x):
         B, S, _ = x.shape  # Batch, Sequence
         H = self.args.n_heads # number of heads
         D = self.args.n_dim  # model dim
@@ -33,22 +33,7 @@ class MultiheadAttention(nn.Module):
 
         q, k, v = qkv.unbind(dim=2)
 
-        '''
-        original code before using flash attention:
-        
-        # Scaled-Dot-Product Attention
-        scores = (q @ k.transpose(-2, -1)) / math.sqrt(D_h)  # (B, H, S, S)
-
-        # mask
-        if mask is not None:
-            scores = scores + mask
-
-        weights = F.softmax(scores, dim=-1)
-        weights = self.dropout(weights)
-        context = weights @ v  # (B, H, S, D_h)
-        '''
-
-        context = F.scaled_dot_product_attention(query=q, key=k, value=v, attn_mask=mask, dropout_p=self.dropout) # using flash attention
+        context = F.scaled_dot_product_attention(query=q, key=k, value=v, attn_mask=None, is_causal=True, dropout_p=self.dropout) # using flash attention
         context = context.transpose(1, 2).contiguous().view(B, S, D)
 
         return context
@@ -79,8 +64,8 @@ class Block(nn.Module):
         self.norm1 = nn.LayerNorm(args.n_dim)
         self.norm2 = nn.LayerNorm(args.n_dim)
 
-    def forward(self, x, mask: Optional[torch.Tensor] = None):
-        x = x + self.attn(self.norm1(x), mask=mask)
+    def forward(self, x):
+        x = x + self.attn(self.norm1(x))
         x = x + self.mlp(self.norm2(x))
         return x
 
@@ -93,28 +78,26 @@ class Transformer(nn.Module):
         self.pos = nn.Embedding(args.max_seq_len, args.n_dim)
         self.norm = nn.LayerNorm(args.n_dim)
         self.blocks = nn.ModuleList([Block(args) for _ in range(args.n_blocks)])
-        self.fc = nn.Linear(args.n_dim, args.vocab_size)
+        self.fc = nn.Linear(args.n_dim, args.vocab_size, bias=False)
 
         self.fc.weight = self.tok.weight # weight tying significantly reduces number of parameters and improves performance
+        self.apply(self._init_weights)
 
     def forward(self, x: torch.Tensor, y: Optional[torch.Tensor] = None):
         B, S = x.shape
-
         h = self.tok(x)  # (B, S, D)
         h = h + self.pos(torch.arange(S, device=x.device))
         h = self.norm(h)
 
-        mask = None
-        if S > 1:
-            mask = torch.full((S, S), float("-inf"), device=x.device)
-            mask = torch.triu(mask, diagonal=1)  # upper-triangular
-
         for block in self.blocks:
-            h = block(h, mask=mask)
+            h = block(h)
 
         logits = self.fc(h)  # (B, S, V)
 
         if y is not None:
+            logits = logits[:, :-1, :].contiguous()
+            y = y[:, 1:].contiguous()
+
             loss = F.cross_entropy(
                 logits.view(-1, logits.size(-1)),
                 y.view(-1)
@@ -122,6 +105,17 @@ class Transformer(nn.Module):
             return loss, logits
 
         return logits
+
+    @staticmethod
+    def _init_weights(m: nn.Module):
+        if isinstance(m, (nn.Linear, nn.Embedding)):
+            nn.init.normal_(m.weight, mean=0.0, std=0.02)
+            if getattr(m, "bias", None) is not None:
+                nn.init.zeros_(m.bias)
+
+        elif isinstance(m, nn.LayerNorm):
+            nn.init.ones_(m.weight)
+            nn.init.zeros_(m.bias)
 
     def generate(self, tokens: List[int], max_token_length: int, device):
         input_tensor = torch.tensor(tokens, device=device).unsqueeze(0)

train.py
@@ -5,6 +5,10 @@ import time
 import numpy as np
 import torch
 import torch.nn.functional as F
+from torch import nn
+
+import wandb
+from torch.nn.utils import clip_grad_norm_
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import get_cosine_schedule_with_warmup
@@ -21,15 +25,32 @@ max_seq_len: int = 1024
 
 # training
 train_iters = 60_000
-eval_iters = 2
-eval_interval = 1000
+eval_iters = 10
+eval_interval = 100
 warmup_frac = 0.1
 batch_size: int = 1
 max_lr = 6e-4
 warmup_steps_percentage = 0.1
 
-#generation
-max_token_len = 100
+wandb.login()
+
+run = wandb.init(
+    project="LLM",
+    # Track hyperparameters and run metadata.
+    config={
+        'n_dim': n_dim,
+        'n_blocks': n_blocks,
+        'n_heads': n_heads,
+        'max_seq_len': max_seq_len,
+        'train_iters': train_iters,
+        'eval_iters': eval_iters,
+        'eval_interval': eval_interval,
+        'warmup_frac': warmup_frac,
+        'batch_size': batch_size,
+        'max_lr': max_lr,
+        'warmup_steps_percentage': warmup_steps_percentage
+    }
+)
 
 def get_device():
     if torch.cuda.is_available():
@@ -55,11 +76,15 @@ model = Transformer(
     max_seq_len=max_seq_len,
     vocab_size=len(tokenizer))).to(device)
 
+print(model.args.vocab_size)
+
 if torch.__version__ >= "2.0" and device == "cuda":
     model.compile()
 
 if os.path.exists('./model.pt'): # load model if existing
     model.load_state_dict(torch.load("model.pt", map_location=device))
+
+print(f"Model parameters: {(sum(param.numel() for param in model.parameters())):,}")
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=max_lr)
 
@@ -75,15 +100,38 @@ val_data = BinDataset(chunk_size=max_seq_len, split="val", device=device)
 train_loader = DataLoader(dataset=train_data, batch_size=batch_size, shuffle=False, pin_memory=cuda) #shuffling would take ages
 val_loader = DataLoader(dataset=val_data, batch_size=batch_size, shuffle=False, pin_memory=cuda)
 
+
+@torch.no_grad()
+def estimate_loss():
+    model.eval()
+    pbar = tqdm(total=eval_iters, desc="Evaluating")
+    total_loss = 0.0
+    val_iter = iter(val_loader)
+    for step_idx in range(eval_iters):
+        x, y = next(val_iter)
+        with ctx:
+            loss, _ = model(x, y)
+
+        total_loss += loss.item()
+        pbar.update(1)
+
+    pbar.close()
+    model.train()
+    return total_loss / eval_iters # avg val loss
+
+
 def train():
     torch.set_float32_matmul_precision('high')
     model.train()
-    pbar = tqdm(total=train_iters, desc="Training step")
-    loss_sum = 0.0
+    pbar = tqdm(total=train_iters, desc="Training")
+    total_loss = 0.0
 
     train_iter = iter(train_loader)
     for step_idx in range(train_iters):
         x, y = next(train_iter)
+
+        x = x.to(device)
+        y = y.to(device)
 
         with ctx:
             loss, _ = model(x, y)
@@ -92,23 +140,36 @@ def train():
 
             if cuda:
                 scaler.scale(loss).backward()
-                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+                norm = clip_grad_norm_(model.parameters(), max_norm=1.0)
                 scaler.step(optimizer)
                 scaler.update()
             else:
                 loss.backward()
-                torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
+                norm = clip_grad_norm_(model.parameters(), max_norm=1.0)
                 optimizer.step()
 
-            scheduler.step()
+        scheduler.step()
 
-            step_loss = loss.item()
-            loss_sum += step_loss
-            avg_loss = loss_sum / (step_idx + 1)
+        step_loss = loss.item()
+        total_loss += step_loss
+        avg_loss = total_loss / (step_idx + 1)
 
-            pbar.update(1)
-            pbar.set_postfix(step_loss=f"{step_loss:.4f}",
-                             avg_loss=f"{avg_loss:.4f}")
+        pbar.update(1)
+        pbar.set_postfix(step_loss=f"{step_loss:.4f}", avg_loss=f"{avg_loss:.4f}")
+
+        wandb.log({
+            "train/loss": step_loss,
+            "train/learning_rate": scheduler.get_last_lr(),
+            "train/norm": norm
+        })
+
+        if (step_idx + 1) % eval_interval == 0:
+            val_loss = estimate_loss()
+            val_perplexity = torch.exp(torch.tensor(val_loss)).item()
+            wandb.log({
+                "val/loss": val_loss,
+                "val/perplexity": val_perplexity
+            })
 
     pbar.close()
 