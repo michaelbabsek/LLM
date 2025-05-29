e3e02c236a47451aabae8859de41d1809d16d857
michaelbabsek <michael@babsek.de>
2025-05-29 20:16:29 +0200

    added comment

--- trainer.py
+++ trainer.py
@@ -109,7 +109,7 @@ class Trainer:
 
             pbar.set_postfix(loss=f"{loss.item():.4f}")
 
-            if (step_idx + 1) % self.cfg.training.eval_interval == 0:
+            if (step_idx + 1) % self.cfg.training.eval_interval == 0: # evaluate the model
                 val_loss = self._evaluate()
                 wandb.log(
                     {
