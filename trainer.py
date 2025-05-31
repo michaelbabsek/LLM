6cba68df1d89c5217cd57042faefc8fbff3efe7f
michaelbabsek <michael@babsek.de>
2025-05-31 17:15:48 +0200

    fixed bug in train

--- trainer.py
+++ trainer.py
@@ -93,7 +93,7 @@ class Trainer:
             step_loss = 0.0
             for micro_step in range(self.cfg.training.grad_accum_steps):
                 loss = self._forward(*self._move(next(self.train_iter))) / self.cfg.training.grad_accum_steps
-                loss.backward()
+                self._backward(loss)
                 step_loss += loss.item()
 
             grad_norm = self._opt_step()
